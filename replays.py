dbfc91e22102a48b0291683fa5138c4d4ea49401
Hamid Abdulghani <73379489+Hamid2250@users.noreply.github.com>
2023-02-25 19:49:11 +0300

    Delete replays.py

--- replays.py
+++
@@ -1,6 +0,0 @@
-def process_message(message, user):
-    ##### English Response #####
-    message = message.lower()
-    print(f"reply {message}")
-    if message in ("hi"):
-        return 'Hello'
