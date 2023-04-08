fd53bc9d58088093c75400d3adbf97f411725a53
unknown <pro.designer2250@gmail.com>
2023-04-08 11:28:43 +0300

    current version 08/04/2023

--- main.py
+++ main.py
@@ -14,25 +14,37 @@ options = webdriver.ChromeOptions()
 options.add_argument(CHROME_DATA_PATH)
 driver = webdriver.Chrome(service=service, options=options)
 driver.get(BASE_URL)
-driver.implicitly_wait(20)
 
 def get_message():
-    # Check for new messages
-    new_message = driver.find_element(By.XPATH, '//*[@id="pane-side"]/div/div/div/div[1]/div/div/div/div[2]/div[2]/div[2]/span[1]/div')
-    new_message.click()
-    driver.implicitly_wait(5)
+    # Get all the chat windows
+    chat_windows = driver.find_elements(By.XPATH, '//*[@id="pane-side"]')
 
-    # Find the chat window element
-    chat_window = driver.find_element(By.XPATH, '//*[@id="main"]/div[2]/div/div[2]/div[3]')
+    # Loop through each chat window
+    for window in chat_windows:
+        # Check if the chat window has a new message
+        new_message_indicator = window.find_element(By.CSS_SELECTOR, 'span[data-testid="icon-unread-count"]')
+        if new_message_indicator:
+            # Click on the chat window to open it
+            new_message_indicator.click()
+            driver.implicitly_wait(5)
 
-    # Get the text of the last message
-    last_message_text = chat_window.find_elements(By.CSS_SELECTOR, 'div[class="message-in focusable-list-item _7GVCb _2SnTA _1-FMR"]')
-    last_message_text = last_message_text[-1].find_element(By.CSS_SELECTOR, 'span[class="_11JPr selectable-text copyable-text"]').text    
-    last_message_text = list(last_message_text.split('\n'))
+            # Find the chat window element
+            chat_window = driver.find_element(By.XPATH, '//*[@id="main"]/div[2]/div/div[2]/div[3]')
+            
+            # Get the text of the last message
+            last_message_text = chat_window.find_elements(By.CSS_SELECTOR, 'div[class="message-in focusable-list-item _7GVCb _2SnTA _1-FMR"]')
+            last_message_text = last_message_text[-1].find_element(By.CSS_SELECTOR, 'div[class="_21Ahp"]')
+            last_message_text = last_message_text.find_element(By.CSS_SELECTOR, 'span[class="_11JPr selectable-text copyable-text"]').text
+            last_message_text = list(last_message_text.split('\n'))
 
-    # Get the user
-    user = driver.find_element(By.CSS_SELECTOR, 'span[class="ggj6brxn gfz4du6o r7fjleex g0rxnol2 lhj4utae le5p0ye3 l7jjieqr _11JPr"]').text
-    return last_message_text, user
+            # Get the user
+            user = driver.find_element(By.CSS_SELECTOR, 'span[class="ggj6brxn gfz4du6o r7fjleex g0rxnol2 lhj4utae le5p0ye3 l7jjieqr _11JPr"]').text
+            if user[0] == '+':
+                user = user[1:]
+            return last_message_text, user
+    
+    # If no new messages are found, return None
+    return None, None
 
 def close_chat():
     # Find the chat header element
@@ -44,17 +56,19 @@ def close_chat():
 
     # Get Close Chat Button and click it
     driver.implicitly_wait(5)
-    close_chat_text = driver.find_element(By.XPATH, '//*[@id="app"]/div/span[4]/div/ul/div/div/li[3]/div')
+    menu_list = driver.find_element(By.CSS_SELECTOR, 'div[class="_2sDI2 _2NU8a"]')
+    close_chat_text = menu_list.find_element(By.CSS_SELECTOR, 'div[aria-label="Close chat"]')
     close_chat_text.click()
     
 
 def reply(message):
     # Type and send message
-    message_box = driver.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]')
+    footer = driver.find_element(By.CSS_SELECTOR, 'footer[class="_3E8Fg"]')
+    message_box = footer.find_element(By.CSS_SELECTOR, 'div[class="to2l77zo gfz4du6o ag5g9lrv bze30y65 kao4egtt"]')
     message_box.click()
-    # driver.implicitly_wait(5)
+    driver.implicitly_wait(5)
     message_box.send_keys(message)
-    # driver.implicitly_wait(5)
+    driver.implicitly_wait(5)
     message_box.send_keys(Keys.ENTER)
     sleep(1)
 
@@ -79,18 +93,20 @@ def send_message(name, message):
     sleep(1)
 
 
+
+
 while True:
     try:
-        driver.implicitly_wait(20)
-        message, user = get_message()
-        message = process_message(message, user)
+        # driver.implicitly_wait(20)
+        messages, user = get_message()
+        if messages is None or user is None:
+            # No new messages found, continue to next iteration
+            continue
+        message = process_message(messages, user)
         reply(message)
         driver.implicitly_wait(5)
         close_chat()
-        # driver.get(BASE_URL)
         # send_message("+966538192825", "Hi John, how are you?")
     except Exception:
         sleep(0.2)
 
-# Close browser
-# driver.quit()
--- replys.py
+++ replys.py
@@ -12,63 +12,61 @@ orders = sheets.worksheet('Orders')
 def process_message(messages, user):
     reply = []
     delivery_note = 'تحضير وإرسال فوري'
+    print(messages)
     
-    # Check if Quotation
-    if messages[0][2] == '1':
-        
-        # Get current time
-        now = datetime.datetime.now()
-        
-        # Get all column names
-        column_names = orders_task_list.row_values(1)
-
-        # Create a dictionary with empty values
-        orders_task_list_default = dict.fromkeys(column_names, '')
-        
-        try:
-            int(messages[-1])
-            for message in messages:
-                if orders_task_list.find(message, in_column=5) or orders.find(message, in_column=1):
-                    reply.append(f"الطلبية رقم {message} مكررة")
-                else:
-                    created_time = now.strftime("%d/%m/%Y %I:%M %p")
-                    update = {'Created by': user, 'Create DateTime': created_time, 'Quotation': message, 'Delivery_Note': delivery_note}
-                    update = dict(orders_task_list_default, **update)
-                    update = list(update.values())
-                    orders_task_list.append_row(update, value_input_option='USER_ENTERED')
-                    reply.append(f"تم إضافة الطلبية {message} وجاري العمل على المطلوب")
-        except Exception:
-            delivery_note = messages[-1]
-            for message in messages[:-1]:
-                if orders_task_list.find(message, in_column=5) or orders.find(message, in_column=1):
-                    reply.append(f"الطلبية رقم {message} مكررة")
-                else:
-                    created_time = now.strftime("%d/%m/%Y %I:%M %p")
-                    update = {'Created by': user, 'Create DateTime': created_time, 'Quotation': message, 'Delivery_Note': delivery_note}
-                    update = dict(orders_task_list_default, **update)
-                    update = list(update.values())
-                    orders_task_list.append_row(update, value_input_option='USER_ENTERED')
-                    reply.append(f"تم إضافة الطلبية {message} وجاري العمل على المطلوب")
-    for message in messages:
-    
-        # English Response 
-        if message in ("هلا"):
-            reply.append('هلا')
-        elif message.lower() in ("hi"):
-            reply.append('Hey!')
-    
-    print('\n'.join(reply))
-    return '\n'.join(reply)
-
-
-
-# message = ['52119010', '52119011', 'تحضير']
-# if message[0][2] == '1':
-#     try:
-#         int(message[-1])
-#         for msg in message:
-#             print(msg)
-#     except Exception:
-#         for msg in message[:-1]:
+    try:
+        int(messages[0])
+        # Check if Quotation
+        if messages[0][2] == '1':
             
-#             print(Exception)
+            # Get current time
+            now = datetime.datetime.now()
+            
+            # Get all column names
+            column_names = orders_task_list.row_values(1)
+
+            # Create a dictionary with empty values
+            orders_task_list_default = dict.fromkeys(column_names, '')
+            
+            try:
+                int(messages[-1])
+                for message in messages:
+                    if orders_task_list.find(message, in_column=5) or orders.find(message, in_column=1):
+                        reply.append(f"الطلبية رقم {message} مكررة")
+                    else:
+                        created_time = now.strftime("%d/%m/%Y %I:%M %p")
+                        update = {'Created by': user, 'Create DateTime': created_time, 'Quotation': message, 'Delivery Note': delivery_note}
+                        update = dict(orders_task_list_default, **update)
+                        update = list(update.values())
+                        orders_task_list.append_row(update, value_input_option='USER_ENTERED')
+                        reply.append(f"تم إضافة الطلبية {message} وجاري العمل على المطلوب")
+                return '\n'.join(reply)
+            
+            except Exception:
+                delivery_note = messages[-1]
+                for message in messages[:-1]:
+                    if orders_task_list.find(message, in_column=5) or orders.find(message, in_column=1):
+                        reply.append(f"الطلبية رقم {message} مكررة")
+                    else:
+                        created_time = now.strftime("%d/%m/%Y %I:%M %p")
+                        update = {'Created by': user, 'Create DateTime': created_time, 'Quotation': message, 'Delivery Note': delivery_note}
+                        update = dict(orders_task_list_default, **update)
+                        update = list(update.values())
+                        orders_task_list.append_row(update, value_input_option='USER_ENTERED')
+                        reply.append(f"تم إضافة الطلبية {message} وجاري العمل على المطلوب")
+                return '\n'.join(reply)
+            
+    
+    except Exception:
+        for message in messages:
+        
+            # English Response 
+            if message in ("هلا"):
+                reply.append('هلا')
+            elif message.lower() in ("hi"):
+                reply.append('Hey!')
+            else:
+                reply.append('مدري')
+        return '\n'.join(reply)
+    
+
